78e5db944dfcf54bbe9ed506f859c096ec5a24cf
jiangyh <1162242058@qq.com>
2024-08-23 16:12:07 +0800

    增加：大坝位移模块功能

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -1,11 +1,13 @@
+import argparse
 import torch
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import nncore
-from nncore.engine import Engine
+from nncore.engine import Engine, set_random_seed, comm
 from pydl.models.classification_model import Mymodel
-from pydl.networks.classification_network import LeNet
+# from pydl.networks.classification_network import LeNet
+from pydl.networks.lenet import LeNet
 
 stages = [
     dict(
@@ -20,6 +22,18 @@ stages = [
 ]
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='config file')
+    parser.add_argument('--checkpoint', help='load a checkpoint')
+    parser.add_argument('--resume', help='resume from a checkpoint')
+    parser.add_argument('--seed', help='random seed')
+    parser.add_argument('--eval', help='evaluation mode', action='store_true')
+    parser.add_argument('--launcher', help='job launcher')
+    args = parser.parse_args()
+    return args
+
+
 def main():
     transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -27,7 +41,7 @@ def main():
 
     train_set = torchvision.datasets.CIFAR10(root='../datasets', train=True,
                                              download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=36,
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=128,
                                                shuffle=True, num_workers=8)
 
     val_set = torchvision.datasets.CIFAR10(root='../datasets', train=False,
@@ -36,16 +50,28 @@ def main():
                                              shuffle=False, num_workers=8)
 
     data_loaders = dict(train=train_loader, val=val_loader)
+
     net = LeNet()
     model = Mymodel(net)
+    launcher = comm.init_dist(launcher=None)
     time_str = nncore.get_timestamp()
     work_dir = nncore.join('../work_dirs', time_str)
+    log_file = nncore.join(work_dir, '{}.log'.format(time_str))
+    logger = nncore.get_logger(log_file=log_file)
+    logger.info(f'Environment info:\n{nncore.collect_env_info()}')
+    logger.info(f'Elastic launcher: {launcher}')
+    # seed = args.seed if args.seed is not None else cfg.get('seed')
+    seed = set_random_seed(seed=None, deterministic=True)
+    logger.info(f'Using random seed: {seed}')
+
     # Initialize and launch engine
     engine = Engine(
         model=model,
         data_loaders=data_loaders,
         stages=stages,
-        work_dir=work_dir
+        work_dir=work_dir,
+        amp=True,
+        seed=seed
     )
     engine.launch()
 
--- examples/mnist.py
+++ examples/mnist.py
@@ -1,8 +1,9 @@
+import argparse
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, Normalize, Resize, ToTensor
 import nncore
-from nncore.engine import Engine
+from nncore.engine import Engine, comm, set_random_seed
 from pydl.models.classification_model import Mymodel
 from pydl.networks.classification_network import LeNet
 
@@ -20,29 +21,49 @@ stages = [
 
 
 def main():
+    launcher = comm.init_dist(launcher=None)
+    time_str = nncore.get_timestamp()
+    work_dir = nncore.join('../work_dirs', time_str)
+    log_file = nncore.join(work_dir, '{}.log'.format(time_str))
+    logger = nncore.get_logger(log_file=log_file)
+    logger.info(f'Environment info:\n{nncore.collect_env_info()}')
+    logger.info(f'Elastic launcher: {launcher}')
+    # seed = args.seed if args.seed is not None else cfg.get('seed')
+    seed = set_random_seed(seed=None, deterministic=True)
+    logger.info(f'Using random seed: {seed}')
+
     # Prepare datasets and the model
     transform = Compose([ToTensor(), Resize(32), Normalize(0.5, 0.5)])
-
     train = MNIST('../datasets', train=True, transform=transform, download=True)
     train_loader = DataLoader(train, batch_size=16, shuffle=True)
-
     val = MNIST('../datasets', train=False, transform=transform, download=True)
     val_loader = DataLoader(val, batch_size=64, shuffle=False)
 
     data_loaders = dict(train=train_loader, val=val_loader)
-    # model = LeNet()
     net = LeNet()
     model = Mymodel(net)
-    time_str = nncore.get_timestamp()
-    work_dir = nncore.join('../work_dirs', time_str)
     # Initialize and launch engine
     engine = Engine(
         model=model,
         data_loaders=data_loaders,
         stages=stages,
-        work_dir=work_dir
+        work_dir=work_dir,
+        amp=True,
+        seed=seed
     )
     engine.launch()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', help='config file')
+    parser.add_argument('--checkpoint', help='load a checkpoint')
+    parser.add_argument('--resume', help='resume from a checkpoint')
+    parser.add_argument('--seed', help='random seed')
+    parser.add_argument('--eval', help='evaluation mode', action='store_true')
+    parser.add_argument('--launcher', help='job launcher')
+    args = parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
